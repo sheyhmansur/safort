874c9b7ec5890b571b3e6139fdc48140cd8c894a
sheyhmansur <92082169+sheyhmansur@users.noreply.github.com>
2022-05-16 12:06:47 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -134,7 +134,7 @@ window.onscroll = function () {
 };
 
 // timer
-let deadline = "2022-05-15";
+let deadline = "2022-06-20";
 const timer = (id, deadline) => {
   const addZero = (num) => {
     if (num <= 9) {
